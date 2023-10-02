013c4c1c3f5753cd0f95a160f93c425100dce454
Nixon <canonalejandro17@gmail.com>
2023-10-02 14:05:41 -0500

    Registro de usuarios, falta iniciar sesion

--- PetTrace/PetTrace/settings.py
+++ PetTrace/PetTrace/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import pymysql
-import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-pymysql.install_as_MySQLdb()
 pymysql.install_as_MySQLdb()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
